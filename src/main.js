0b70aaefdf6f3ec812a03bc4a354b56e2bc725fa
robz <rjnevels@utexas.edu>
2015-01-18 11:23:11 -0800

    automated ES6, flow, JSX, CommonJS transforms, and packaging

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* @flow */
+
+// CommonJS test
+var React = require('react');
+
+// ES6 test
+var f = (y) => y+3;
+
+// flow test
+var y:number = 3;
+
+// JSX test
+var x = <div>hi</div>;
+
+console.log('yay!');
