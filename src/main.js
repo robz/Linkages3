20a0b94a36360d8dcd57d21ee329616769622e77
robz <rjnevels@utexas.edu>
2016-03-13 22:20:15 -0700

    added ability to save state in url

--- src/main.js
+++ src/main.js
@@ -9,6 +9,24 @@ var UIState = require('./ui/UIState');
 
 var linkageData = require('./linkageData.js');
 
+var urlData;
+try {
+  var s = window.location.search.substring(1);
+  urlData = JSON.parse(window.unescape(s));
+} catch (e) {}
+
+if (urlData) {
+  linkageData = urlData;
+}
+
+document.getElementById('button').onclick = function () {
+  var location = window.location;
+  var url = location.search
+    ? location.href.split(location.search)[0]
+    : location.href;
+  location.href = url + '?' + window.escape(JSON.stringify(ui.state.linkage.spec));
+}
+
 var linkage = new Linkage(linkageData);
 var initialState = UIState.getInitialUnpausedState(linkage);
 var renderer = new LinkageRenderer('mycanvas');
